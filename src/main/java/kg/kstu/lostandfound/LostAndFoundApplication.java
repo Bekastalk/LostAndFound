1b34cdb21c4570e74290d3032d3e44ef1ffb3d64
Bekmamat <azamatovbekmamat518@gmail.com>
2023-11-15 12:25:00 +0600

    add entity  User

--- src/main/java/kg/kstu/lostandfound/LostAndFoundApplication.java
+++ src/main/java/kg/kstu/lostandfound/LostAndFoundApplication.java
@@ -1,4 +1,4 @@
-package com.example.lostandfound;
+package kg.kstu.lostandfound;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
