2b4bc358e7bcb514881bd8f50c395a87492b8fff
Bekmamat <azamatovbekmamat518@gmail.com>
2023-12-23 17:12:38 +0600

    Added entity Listing and repositories

--- src/main/java/kg/kstu/lostandfound/entity/User.java
+++ src/main/java/kg/kstu/lostandfound/entity/User.java
@@ -1,4 +1,30 @@
 package kg.kstu.lostandfound.entity;
 
+import jakarta.persistence.*;
+import kg.kstu.lostandfound.enums.Role;
+import lombok.*;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+    generator = "user_gen")
+    @SequenceGenerator(
+            name = "user_gen",
+            sequenceName = "user_seq",
+            allocationSize = 1
+    )
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @ManyToOne
+    private Listing listing;
 }
---
+++ src/main/java/kg/kstu/lostandfound/enums/Category.java
@@ -0,0 +1,7 @@
+package kg.kstu.lostandfound.enums;
+
+public enum Category {
+    LOSTITEM,
+    FOUNDITEM
+
+}
